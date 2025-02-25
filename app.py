95f7e2b760d01309ded78f76018273802c5cc268
Ishwarya Suresh <109035230+Ishwaryaprogrammer@users.noreply.github.com>
2025-02-25 22:14:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,6 @@ def generate():
 
     return jsonify({'instructions': instructions})
 
+port = int(os.environ.get("PORT", 10000))  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
